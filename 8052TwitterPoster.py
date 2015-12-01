c054f538f2b39f4a06cdbdf9c89b4225a16cd84c
Alex Walls <idrivebears@gmail.com>
2015-12-01 14:03:28 -0600

    serial com working

--- 8052TwitterPoster.py
+++ 8052TwitterPoster.py
@@ -1,33 +1,45 @@
 import time
 import serial
 from twitter import *
+from twython import Twython
+
+#https://twitter.com/8052MicroLopez
+#marioalex
 
 serConnection = serial.Serial(
-    port='/dev/ttyUSB1',
+    port='COM1',
     baudrate=9600,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.EIGHTBITS
 )
 
-
 config = {}
 execfile("config.py", config)
-twitter = Twitter(auth = OAuth(config["acces_key"], config["access_secret"], config["consumer_key"], config["consumer_secret"]))
+twitter = Twython(config['token'], config['token_key'], config['con_secret'], config['con_secret_key'])
+time.sleep(5)
+twitter.update_status(status='MOV TWITTER, HELLO_WORLD')
+#twitter = Twitter(auth = OAuth(config['token'], config['token_key'], config['con_secret'], config['con_secret_key']))
+
+#result = twitter.statuses.update(status = "Hello world?")
 
 def tweet_message(message):
-    result = twitter.statuses.update(status = message)
+    #result = twitter.statuses.update(status = message)
     print("Status posted")
+    time.sleep(3)
 
+print("Application running...")
 while(True):
-    print("Application running...")
+
     print("Awaiting serial input...")
-    
-    message = ''
 
-    while ser.inWaiting() > 0:
-        message += ser.read(1)
+    message = ""
 
-    if message not '':
-        tweet_message(message)
+    while serConnection.inWaiting() > 0:
+        message += serConnection.read(1)
+    print(message)
+    #tweet_message(message)
+
+    #if not message:
+    #    tweet_message(message)
     time.sleep(1)
--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 #   https://apps.twitter.com/app/new
 
-consumer_key = "XxXxXxxXXXxxxxXXXxXX"
-consumer_secret = "xXXXXXXXXxxxxXxXXxxXxxXXxXxXxxxxXxXXxxxXXx"
-access_key = "XXXXXXXX-xxXXxXXxxXxxxXxXXxXxXxXxxxXxxxxXxXXxXxxXX"
-access_secret = "XxXXXXXXXXxxxXXXxXXxXxXxxXXXXXxXxxXXXXx"
+con_secret = "woSvH8DZF6q5X68CcYslwNlAh"
+con_secret_key = "3fNIFWVVMRBhApmeUeAo4OhyzLHu6i3zlgbkEOuk1b2oGMYBLd"
+token = "4341656860-tzWR2CvFMV5tSI0UQimXLKJab01KSsvmDYkge1Y"
+token_key = " aZ82mJzoW7uP7dsol7DSIV5AbMNFINJHDEhNBK2qUFnE3"
