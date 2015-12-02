f10848589f3a409abf000ee32d462b44a7523566
Alex Walls <idrivebears@gmail.com>
2015-12-02 11:32:40 -0600

    Final, should be working

--- 8052TwitterPoster.py
+++ 8052TwitterPoster.py
@@ -5,26 +5,21 @@ from twython import Twython
 
 #https://twitter.com/8052MicroLopez
 #marioalex
-
+'''
 serConnection = serial.Serial(
-    port='COM1',
+    port='COM3',
     baudrate=9600,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.EIGHTBITS
 )
-
+'''
 config = {}
 execfile("config.py", config)
 twitter = Twython(config['token'], config['token_key'], config['con_secret'], config['con_secret_key'])
-time.sleep(5)
-twitter.update_status(status='MOV TWITTER, HELLO_WORLD')
-#twitter = Twitter(auth = OAuth(config['token'], config['token_key'], config['con_secret'], config['con_secret_key']))
-
-#result = twitter.statuses.update(status = "Hello world?")
 
 def tweet_message(message):
-    #result = twitter.statuses.update(status = message)
+    twitter.update_status(status=message)
     print("Status posted")
     time.sleep(3)
 
@@ -37,9 +32,9 @@ while(True):
 
     while serConnection.inWaiting() > 0:
         message += serConnection.read(1)
-    print(message)
-    #tweet_message(message)
 
-    #if not message:
-    #    tweet_message(message)
+    if not message:
+        tweet_message(message)
+        print(message)
+
     time.sleep(1)
--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 #   https://apps.twitter.com/app/new
 
-con_secret = "woSvH8DZF6q5X68CcYslwNlAh"
-con_secret_key = "3fNIFWVVMRBhApmeUeAo4OhyzLHu6i3zlgbkEOuk1b2oGMYBLd"
-token = "4341656860-tzWR2CvFMV5tSI0UQimXLKJab01KSsvmDYkge1Y"
-token_key = " aZ82mJzoW7uP7dsol7DSIV5AbMNFINJHDEhNBK2qUFnE3"
+con_secret = "4341656860-tzWR2CvFMV5tSI0UQimXLKJab01KSsvmDYkge1Y"
+con_secret_key = "aZ82mJzoW7uP7dsol7DSIV5AbMNFINJHDEhNBK2qUFnE3"
+token = "woSvH8DZF6q5X68CcYslwNlAh"
+token_key = "3fNIFWVVMRBhApmeUeAo4OhyzLHu6i3zlgbkEOuk1b2oGMYBLd"
